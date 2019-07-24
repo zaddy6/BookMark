3a564cc80c3d17d1e94bd798fffdaa40ae137ec1
Mac <mac@iMac.local>
2019-07-24 17:05:40 +0300

    added threading support

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -5,7 +5,9 @@
 import http.server
 import requests
 import os
+import threading
 from urllib.parse import unquote, parse_qs
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -28,6 +30,9 @@ form = '''<!DOCTYPE html>
 </pre>
 '''
 
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 def CheckURI(uri, timeout=5):
     '''Check whether this URI is reachable, i.e. does it return a 200 OK?
@@ -109,5 +114,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
